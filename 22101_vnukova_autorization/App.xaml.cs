81275a82eece6c99d54ca095328a17dd65addb1e
pr-22.101-vk <pr-22.101-vk@115-10>
2025-01-23 16:53:44 +0700

    Добавьте файлы проекта.

---
+++ 22101_vnukova_autorization/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _22101_vnukova_autorization
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 22101_vnukova_autorization/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using _22101_vnukova_autorization.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _22101_vnukova_autorization
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text.Trim();
+            string password = PasswordTextBox.Text.Trim();
+
+            using (var context = new KRvnukovaEntities1())
+            {
+                var user = context.Polzovateli.Where(a => a.Login == login && a.Password == password).FirstOrDefault();
+
+                if (user != null)
+                {
+                    MessageBox.Show("Добро пожаловать! Ваша роль: " + user.roles.name);
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
